2298f1f50ca6430c2e14a19b17f41305d38284d4
Alice <alicegabriela2102@gmail.com>
2024-03-07 15:01:44 -0300

    Create desafio tecnico final.cs

---
+++ desafio tecnico final.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+class lightsaberfans
+{
+static void Main()
+{
+string AnakinSkywalkerDarthVaderBenSoloKyloRenObiWanKenobiLukeSkywalkerLeiaOrganaYodaR2D2C3PBB8HanSoloChewbaccaPadméAmidala_MaceWindu_QuiGonJinn_ViceAlmiranteHoldo_RoseTico_PoeDameron_LandoCalrissian_DarthMaul_BobaFett_JabbatheHutt_JangoFett_CondeDookuDarthTyranus_GeneralGrievous_SheevPalpatineDarthSidious_Finn_MazKanata_ReyadotaosobrenomeSkywalker_LíderSupremoSnoke = null;
+string senha = AnakinSkywalkerDarthVaderBenSoloKyloRenObiWanKenobiLukeSkywalkerLeiaOrganaYodaR2D2C3PBB8HanSoloChewbaccaPadméAmidala_MaceWindu_QuiGonJinn_ViceAlmiranteHoldo_RoseTico_PoeDameron_LandoCalrissian_DarthMaul_BobaFett_JabbatheHutt_JangoFett_CondeDookuDarthTyranus_GeneralGrievous_SheevPalpatineDarthSidious_Finn_MazKanata_ReyadotaosobrenomeSkywalker_LíderSupremoSnoke;
+long usuario = 97110100101114121; // andery em numero de texto binario 
+string tentativa_senha;
+long tentativa_usuario;
+bool Errado = false;
+bool verdadeiro = true;
+Console.WriteLine("Olá Bem vindo a Rede social lightsaberfans");
+Console.WriteLine("Para entrar coloque a sua senha e usuário");
+Console.WriteLine("Digite o usuário");
+tentativa_usuario = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a senha");
+tentativa_senha = Console.ReadLine();
+if (tentativa_usuario == usuario) {
+
+true;
+Console.WriteLine("Usuario certo");
+if (tentativa_senha == senha)
+{
+Console.WriteLine("Parabens por conseguir entrar no melhor aplicativo para falar com os seus amigos jedis e mostrar dados da sua batalha");
+Console.WriteLine("Insira o quantidade de slides que vc tem no momento com stars wars envolvido e quantos sabres de luzes que e  tem para calcularmos o quão fã vc é");
+Console.WriteLine("Insira o quantidade de slides que vc tem no momento com stars wars envolvido ate o momento");
+float slide = float.Parse(Console.ReadLine()); // float porque vc pode estar no meio de um slide
+Console.WriteLine("quantos sabres de luzes que voce tem");
+int sabre = int.Parse(Console.ReadLine());
+float  resultado;
+resultado = slide * sabre;
+if (resultado<=2){
+    Console.WriteLine("Voce não é tão fa de star wars, va assistir mais filmes");
+
+}
+else if (2< resultado <= 5){
+    Console.WriteLine("Voce é fã de stars wars, parabens");
+
+}
+if (resultado>5){
+    Console.WriteLine("Voce né muito fa de star wars, acho que estou falando com um jedi ");
+}
+else if(){
+    
+}
+else if 
+{
+false;
+Console.WriteLine("Usuario errado");
+break;
+}
+
+
+}
+}
+}
+}
